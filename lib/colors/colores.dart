cad0f55bf101b35eecda65db45be3cdca6000aa7
ManuelFulls <dediosdiego606@gmail.com>
2024-11-02 16:21:39 -0600

    Se ha desarrollado la pantalla inicial de la aplicacion,la de bienvenida

---
+++ lib/colors/colores.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF03045E);
+const Color secondaryColor = Color(0xFF0077B6);
+const Color thirdColor = Color(0xFF00B4D8);
