c3c497f36b46a00d0b2159ed75075dab476635b9
ManuelFulls <dediosdiego606@gmail.com>
2024-11-02 09:13:48 -0600

    TITULO

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Aplicación de Notas'),
+      home: const MyHomePage(title: 'Aplicación de Notas ESCRIBE TA'),
     );
   }
 }
